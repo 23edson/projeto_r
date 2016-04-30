dfcc5dc3ccfac5451b788034803adfbf1caec01b
Edson <ed_edsoon@hotmail.com>
2016-04-29 22:08:30 -0300

    mesm

--- RU/cadastroPratos.php
+++ RU/cadastroPratos.php
@@ -1,7 +1,23 @@
-
-
 <!DOCTYPE html>
 <html  dir="ltr" lang="pt-br" xml:lang="pt-br">
+<?php
+	#session_start();
+
+#$_SESSION = array();
+#	if(isset($_SESSION['flag'])){
+	#		if($_SESSION['flag'])
+	#			echo '<div  style="center: 78px; height: 455px; left: 208px; top: 400px; width: 523px">inserido com sucesso</div>';
+		#		echo 'aQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQDLDDLDL';
+		#		header('Location: index.php');
+#	}
+	#else{
+	#$test = $_GET['flag'];
+	#if(isset($test))
+	##	if($test)echo '<div>inserido com sucesso</div>';
+  #else{
+ ?>
+
+
 <head>
 	 <link href="css/bootstrap.min.css" rel="stylesheet">
        <link rel="stylesheet" type="text/css" href="tela.css" />
@@ -9,7 +25,7 @@
     <title>Cardápio Online Restaurante Universitário - Universidade Federal da Fronteira Sul</title>
     <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
         <meta name="author" content="pcmaster" />
-        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="shortcut icon" href="../favicon.ico">
         <link rel="stylesheet" type="text/css" href="css/demo.css" />
         <link rel="stylesheet" type="text/css" href="css/style.css" />
         <link rel="stylesheet" type="text/css" href="css/animate-custom.css" />
@@ -17,26 +33,30 @@
 
     <meta name="description" content="">
     <meta name="author" content="pcmaster" >
- 
-   
+
+
     <link href="css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
     <script src="js/jquery.min.js"></script>
     <script src="js/fileinput.js" type="text/javascript"></script>
-   
+
     <script src="js/bootstrap.min.js" type="text/javascript"></script>
 	<script src="js/bootstrap-datepicker.js"></script>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 
 	<script type="text/javascript">
-	
-		
+
+
 		function getpos() {
-			
-		
+
+
 		var div = document.getElementById('div3');
 		div.style.top = '120px';
 		//print("rwinreireionoineionenoinoinro3in")
+		}
+
+		function displayf(){
+
 		}
 	</script>
 
@@ -48,17 +68,17 @@
 
 <div class="skiplinks">
 
-<header role="banner" class="navbar navbar-fixed-top navbar-inverse moodle-has-zindex" body style="background:#228B22">
-        <div class="container-fluid">
-	     
-            <img src="http://uffs.edu.br/images/identidadevisual/Chapeco/IdentidadeVisual_Campus_Chapec_Colorida_para_fundos_Escuros.png" border="000" width="100" height="120" alt="Logo UFFS" title="Logo Universidade Federal da Fronteira Sul" /></div></header>
-     <br><br>   <br><br>  <br><br>      
+<header role="banner" class="navbar navbar-fixed-top navbar-inverse moodle-has-zindex" body style="background:#228B21;border:#228B21;padding:2px" >
+        <div class="container-fluid" padding="2px">
+
+            <img src="http://uffs.edu.br/images/identidadevisual/Chapeco/IdentidadeVisual_Campus_Chapec_Colorida_para_fundos_Escuros.png" width="100" height="120" alt="Logo UFFS" title="Logo Universidade Federal da Fronteira Sul" /></div></div></header>
+     <br><br>   <br><br>  <br><br>
      <div class="container">
 
-	
 
 
-	<section>				
+
+	<section>
                 <div id="container_demo" >
                     <!-- hidden anchor to stop jump http://www.css3create.com/Astuce-Empecher-le-scroll-avec-l-utilisation-de-target#wrap4  -->
                     <a class="hiddenanchor" id="toregister"></a>
@@ -67,15 +87,21 @@
                         <div id="login"  class="animate form">
 
       				<form action="upload.php" method="post" enctype="multipart/form-data">
-		
+
         			 <header><h1 style="color: #228B22" ><span>Cadastrar Prato</span></h1></header>
 
-				 <p> 
-                                    <label for="inputCard" style="color: #228B22" data-icon="" >Nome</label>
-                                    <input id="inputCard" name="inputCard" placeholder="" required="true"/>
+				                         <p>
+                                    <label for="inputpra" style="color: #228B22" data-icon="" >Nome</label>
+                                    <input id="inputpra" name="inputpra" placeholder="" required="true"/>
                                 </p>
-				
-				
+
+																	<p>
+																					<label for="inputipo" style="color: #228B22" data-icon="" >Tipo</label>
+																				<input id="inputipo" name="inputipo" placeholder="" required="true"/>
+																	</p>
+
+
+
     				<input id="input-23" name="input-23" type="file" multiple="false" class="file-loading" Onclick="getpos()" >
     				<script>
     					$(document).on('ready', function() {
@@ -91,38 +117,38 @@
                								 "<div class=\'input-group {class}\'>\n" +
                								 "   <div class=\'input-group-btn\'>\n" +
                 						"       {browse}\n" +
-                       
-                
+
+
                 						"   </div>\n" +
                 						"   {caption}\n" +
                							 "</div>"
             						}
        						 });
-							 
+
     						});
     				</script>
 				<br><br>
-				
-			
+
+
       				<p >
-                                    <input class="btn btn-success" type="submit" value="Adicionar" name="fsub" id="fsub" Onclick="<?php $_SESSION['atualiza'] = 0;?>" /> 
+                                    <input class="btn btn-success" type="submit" value="Adicionar" name="fsub" id="fsub" Onclick="<?php $_SESSION['atualiza'] = 0;?>" />
 				</p>
 
 			</form>
 
 
 	         </div>
-		
-		
+
+
         	</div>
 			</div>
-		
-	
+
+
         </section>
-	
-        
-     
-  
+
+
+
+
 
     </div> <!-- /container -->
 
@@ -131,14 +157,12 @@
 
  <div id="div3"><p style="text-align:center;"><b>
         	<br><br>
-        	
+
         	<br>
-        	
+
         	<img id="comida" src="comida.jpg" width="400" height="120" alt="Logo UFFS" title="Logo Universidade Federal da Fronteira Sul"/>
         </div>
-        
+
 
 </body>
 </html>
-
-
--- RU/upload.php
+++ RU/upload.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+session_start();
+
+$_SESSION['flag'] = 0;
 require("connect.php");
 
 
@@ -9,12 +12,13 @@ if(isset($_POST['fsub'])){
 	$_UP['tamanho'] = 1000000; //Aprox 1mb
 
 
-	$card = $_POST['inputCard'];
+	$card = $_POST['inputpra'];
+	$tipo = $_POST['inputipo'];
 
 	$nome = $_FILES['input-23']['name'];
-		
+
 			$arquivo_tmp = $_FILES['input-23']['tmp_name'];
-    
+
 			//$extensao = strtolower(end(explode('.', $_FILES['input-23']['name'])));
 			$extensao = strrchr($nome, '.');
 			// Pega a extensao
@@ -22,21 +26,24 @@ if(isset($_POST['fsub'])){
 
 			// Converte a extensao para minusculo
 			$extensao = strtolower($extensao);
-			
-			
+
+
 			$novoNome = md5(microtime()) .$extensao;
-			
+
 			$destino = $_UP['pasta'] . $novoNome;
 			//echo $destino;
 			if(move_uploaded_file( $arquivo_tmp, $destino)){
 
-				$query = "insert into Pratos (id,nome,img,id_pessoa) values  (NULL, '".$card."', '".$destino."', '"."3"."')";
+				$query = "insert into Pratos (id,nome,tipo,img) values  (NULL, '".$card."','".$tipo."', '".$destino."' )";
 				$result = mysql_query($query);
 				if(!$result){ //se tiver problemas, retorna falso
-					print"erro";
+					print"PROBLEM DETECTED";
+					header('Location: tela.php');
 				}
-				echo '<div class="success">Cardápio salvo com sucesso.</div>';
-				
+				else {echo '<div class="success">Cardápio salvo com sucesso.</div>';
+						$_SESSION['flag']=1;
+							header('Location: tela.php');
+				}
 
 
 			}
